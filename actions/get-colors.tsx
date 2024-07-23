bcc638995b8963d2e78155e777376754032f347e
Nikhil chopra <nikhil0080012005@gmail.com>
2024-07-23 11:59:12 +0530

    error resolved

--- actions/get-colors.tsx
+++ actions/get-colors.tsx
@@ -1,10 +1,14 @@
 import { Color } from "@/types";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`;
 
-const getColor = async (): Promise<Color> => {
-    const res = await fetch(`${URL}`);
-    return res.json();
-}
+const getColor = async (): Promise<Color[]> => {
+    const res = await fetch(URL);
+    if (!res.ok) {
+        throw new Error('Failed to fetch colors');
+    }
+    const colors: Color[] = await res.json();
+    return colors;
+};
 
 export default getColor;
--- actions/get-sizes.tsx
+++ actions/get-sizes.tsx
@@ -1,10 +1,14 @@
 import { Size } from "@/types";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes`
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes`;
 
-const getSize = async (): Promise<Size> => {
-    const res = await fetch(`${URL}`);
-    return res.json();
-}
+const getSize = async (): Promise<Size[]> => {
+    const res = await fetch(URL);
+    if (!res.ok) {
+        throw new Error('Failed to fetch sizes');
+    }
+    const sizes: Size[] = await res.json();
+    return sizes;
+};
 
 export default getSize;
